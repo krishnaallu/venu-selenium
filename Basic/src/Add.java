c09f044ff94fb849ba7287354275136f1b05c96f
venu <venuchalla1993@gmail.com>
2018-07-09 21:10:13 -0500

    added subtraction method

---
+++ Basic/src/Add.java
@@ -0,0 +1,18 @@
+
+public class Add {
+
+	void subtraction(int c,int d) {
+		
+		int e=c-d;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		int b=1;
+		System.out.println(a+b);
+		Add add= new Add();
+		add.subtraction(9,7);
+
+	}
+
+}
